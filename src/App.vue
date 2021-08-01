50e28a0136506146602fa57acb4fc491a403030b
Evgeniy Kalimullin <counsil1986@gmail.com>
2021-08-01 22:12:13 +0700

    homework mod3.4

--- src/App.vue
+++ src/App.vue
@@ -1,91 +1,18 @@
 <template>
   <div>
-    <section class="catalog">
-      <ul class="catalog__list">
-        <li class="catalog__item" v-for="product in products" :key="product.id">
-          <a class="catalog__pic" href="#">
-            <img
-              :src="product.imageUrl"
-              :srcset="`${product.imageUrl.split('.')[0]}@2x.${product.imageUrl.split('.')[1]} 2x`"
-              alt="Название товара"
-            />
-          </a>
-
-          <h3 class="catalog__title">
-            <a href="#"> {{ product.title }} </a>
-          </h3>
-
-          <span class="catalog__price"> {{ product.price }} ₽ </span>
-
-          <ul class="colors colors--black" v-if="product.colors.length">
-            <li class="colors__item" v-for="(color, index) in product.colors" :key="index">
-              <label class="colors__label">
-                <input class="colors__radio sr-only" type="radio" name="color-1" :value="`#${color}`" checked="" />
-                <span class="colors__value" :style="`background-color: #${color}`"> </span>
-              </label>
-            </li>
-          </ul>
-          <ul class="sizes" v-if="product.sizes.length">
-            <li class="sizes__item" v-for="size in product.sizes" :key="size">
-              <label class="sizes__label">
-                <input class="sizes__radio sr-only" type="radio" name="sizes-1" :value="size" />
-                <span class="sizes__value">
-                  {{ size }}
-                </span>
-              </label>
-            </li>
-          </ul>
-        </li>
-      </ul>
-
-      <ul class="catalog__pagination pagination">
-        <li class="pagination__item">
-          <a
-            class="pagination__link pagination__link--arrow pagination__link--disabled"
-            aria-label="Предыдущая страница"
-          >
-            <svg width="8" height="14" fill="currentColor">
-              <use xlink:href="#icon-arrow-left"></use>
-            </svg>
-          </a>
-        </li>
-        <li class="pagination__item">
-          <a class="pagination__link pagination__link--current"> 1 </a>
-        </li>
-        <li class="pagination__item">
-          <a class="pagination__link" href="#"> 2 </a>
-        </li>
-        <li class="pagination__item">
-          <a class="pagination__link" href="#"> 3 </a>
-        </li>
-        <li class="pagination__item">
-          <a class="pagination__link" href="#"> 4 </a>
-        </li>
-        <li class="pagination__item">
-          <a class="pagination__link" href="#"> ... </a>
-        </li>
-        <li class="pagination__item">
-          <a class="pagination__link" href="#"> 10 </a>
-        </li>
-        <li class="pagination__item">
-          <a class="pagination__link pagination__link--arrow" href="#" aria-label="Следующая страница">
-            <svg width="8" height="14" fill="currentColor">
-              <use xlink:href="#icon-arrow-right"></use>
-            </svg>
-          </a>
-        </li>
-      </ul>
-    </section>
+    <Header />
+    <Main />
+    <Footer />
   </div>
 </template>
 
 <script>
-import products from './data/products'
+import Header from './components/header/Header.vue'
+import Main from './components/main/Main.vue'
+import Footer from './components/footer/Footer.vue'
 export default {
   name: 'App',
-  data: () => ({
-    products,
-  }),
+  components: { Header, Main, Footer },
 }
 </script>
 
