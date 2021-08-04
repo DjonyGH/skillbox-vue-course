1994733bb390c9f14501c320bdb00fd959f82322
Evgeniy Kalimullin <counsil1986@gmail.com>
2021-08-04 23:26:36 +0700

    homework mod.4.4

--- src/components/main/catalogList/CatalogList.vue
+++ src/components/main/catalogList/CatalogList.vue
@@ -1,40 +1,7 @@
 <template>
   <section class="catalog">
     <ul class="catalog__list">
-      <li class="catalog__item" v-for="product in products" :key="product.id">
-        <a class="catalog__pic" href="#">
-          <img
-            :src="product.imageUrl"
-            :srcset="`${product.imageUrl.split('.')[0]}@2x.${product.imageUrl.split('.')[1]} 2x`"
-            alt="Название товара"
-          />
-        </a>
-
-        <h3 class="catalog__title">
-          <a href="#"> {{ product.title }} </a>
-        </h3>
-
-        <span class="catalog__price"> {{ product.price }} ₽ </span>
-
-        <ul class="colors colors--black" v-if="product.colors.length">
-          <li class="colors__item" v-for="(color, index) in product.colors" :key="index">
-            <label class="colors__label">
-              <input class="colors__radio sr-only" type="radio" name="color-1" :value="`#${color}`" checked="" />
-              <span class="colors__value" :style="`background-color: #${color}`"> </span>
-            </label>
-          </li>
-        </ul>
-        <ul class="sizes" v-if="product.sizes.length">
-          <li class="sizes__item" v-for="size in product.sizes" :key="size">
-            <label class="sizes__label">
-              <input class="sizes__radio sr-only" type="radio" name="sizes-1" :value="size" />
-              <span class="sizes__value">
-                {{ size }}
-              </span>
-            </label>
-          </li>
-        </ul>
-      </li>
+      <ProductItem v-for="product in products" :key="product.id" :product="product" />
     </ul>
 
     <Pagination
@@ -49,10 +16,11 @@
 
 <script>
 import products from '@/data/products'
-import Pagination from '../pagination/Pagination.vue'
+import ProductItem from './productItem/ProductItem.vue'
+import Pagination from './pagination/Pagination.vue'
 export default {
   name: 'CatalogList',
-  components: { Pagination },
+  components: { Pagination, ProductItem },
   data: () => ({
     page: 1,
     productsPerPage: 3,
---
+++ src/components/main/catalogList/productItem/ProductItem.vue
@@ -0,0 +1,50 @@
+<template>
+  <li class="catalog__item">
+    <a class="catalog__pic" href="#">
+      <img
+        :src="product.imageUrl"
+        :srcset="`${product.imageUrl.split('.')[0]}@2x.${product.imageUrl.split('.')[1]} 2x`"
+        alt="Название товара"
+      />
+    </a>
+
+    <h3 class="catalog__title">
+      <a href="#"> {{ product.title }} </a>
+    </h3>
+
+    <span class="catalog__price"> {{ product.price }} ₽ </span>
+
+    <ul class="colors colors--black" v-if="product.colors.length">
+      <li class="colors__item" v-for="(color, index) in product.colors" :key="index">
+        <label class="colors__label">
+          <input class="colors__radio sr-only" type="radio" :value="`#${color}`" v-model="selectedColor" />
+          <span class="colors__value" :style="`background-color: #${color}`"> </span>
+        </label>
+      </li>
+    </ul>
+    <ul class="sizes" v-if="product.sizes.length">
+      <li class="sizes__item" v-for="size in product.sizes" :key="size">
+        <label class="sizes__label">
+          <input class="sizes__radio sr-only" type="radio" name="sizes-1" :value="size" />
+          <span class="sizes__value">
+            {{ size }}
+          </span>
+        </label>
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'ProductItem',
+  props: ['product'],
+  data() {
+    return {
+      selectedColor: `#${this.product.colors[0]}`,
+    }
+  },
+}
+</script>
+
+<style></style>
