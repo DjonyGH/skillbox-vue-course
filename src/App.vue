a3b0f61bdbff87726f74174e4a1e786d40c51cd9
Evgeniy Kalimullin <counsil1986@gmail.com>
2021-08-08 19:36:30 +0700

    homework mod.5.1

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,30 @@
 <template>
   <div>
     <Header />
-    <Main />
+    <Component :is="currentPageComponent" :page-params="currentPageParams" />
     <Footer />
   </div>
 </template>
 
 <script>
-import Header from './components/header/Header.vue'
-import Main from './components/main/Main.vue'
-import Footer from './components/footer/Footer.vue'
+import Header from '@/components/header/Header.vue'
+import Footer from '@/components/footer/Footer.vue'
+import MainPage from '@/pages/MainPage.vue'
+import ProductPage from '@/pages/ProductPage.vue'
+import NotFoundPage from '@/pages/NotFoundPage.vue'
+import routes from '@/routes/routes.js'
 export default {
   name: 'App',
-  components: { Header, Main, Footer },
+  components: { Header, Footer, MainPage, ProductPage, NotFoundPage },
+  data: () => ({
+    currentPage: 'main',
+    currentPageParams: {},
+  }),
+  computed: {
+    currentPageComponent() {
+      return routes[this.currentPage] || 'NotFoundPage'
+    },
+  },
 }
 </script>
 
